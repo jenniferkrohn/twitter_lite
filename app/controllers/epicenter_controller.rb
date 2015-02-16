730f2801e2f5a728dc66f4fb36ee367e3236b6e7
Jennifer Krohn <jenniferlynn1207@msn.com>
2015-02-16 11:41:29 -0500

    first commit

---
+++ app/controllers/epicenter_controller.rb
@@ -0,0 +1,33 @@
+class EpicenterController < ApplicationController
+  def feed
+  	@following_tweets = []
+
+  	@tweets = Tweet.all
+
+  	@tweets.each do |tweet|
+  		if current_user.following.include?(tweet.user.id)
+  			@following_tweets.push(tweet) 
+  		end
+  	end
+  end
+
+  def show_user
+  	@user = User.find(params[:id])
+  end
+
+  def now_following
+  	@user = User.find(params[:follow_id])
+
+  	current_user.following.push(params[:follow_id].to_i)
+
+  	current_user.save
+  end
+
+  def unfollow
+    @user = User.find(params[:unfollow_id])
+
+    current_user.following.delete(params[:unfollow_id].to_i)
+
+    current_user.save
+  end
+end
